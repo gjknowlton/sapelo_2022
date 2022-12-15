4e857ecffa8acdde96bcd228620ac722107d023d
gjknowlton <gknowlt1@gmail.com>
2022-12-15 15:46:40 -0600

    add scripts and data

--- data_analyses.R
+++ data_analyses.R
@@ -3,22 +3,64 @@
 
 ####  load packages ####
 
-#install.packages("pacman")
+#install.packages("compensateR")
 library(pacman)
-p_load(tidyverse, lme4, car, vegan, ggpubr)
+p_load(tidyverse, lme4, car, vegan, ggpubr, compensateR)
+
+library(parameters)
+library(see)
+library(sjPlot)
+library(ggplot2)
+library(report)
+
 
 ####  read & tidy data, calculate diversity indices ####
 
-covariates <- read.csv("G:\\My Drive\\Classes\\Sapelo\\Data\\covariates_all.csv")
-comm.data <-  read.csv("G:\\My Drive\\Classes\\Sapelo\\diversity_calcs_data.csv")
-total_cover <- read.csv("G:\\My Drive\\Classes\\Sapelo\\total_pct_cover.csv")
+covariates <- read.csv("data/covariates_all.csv")
+comm.data <-  read.csv("data/diversity_calcs_data.csv")
+total_cover <- read.csv("data/total_pct_cover.csv")
+
+# Convert specific conductance to salinity (ppt)
+
+salinity <- function(salinity){
+  sc <- c(salinity)
+  
+  R <- sc/53087
+  
+  k1 <- 0.0120
+  k2 <- -0.2174
+  k3 <- 25.3283
+  k4 <- 13.7714
+  k5 <- -6.4788
+  k6 <- 2.5842
+  
+  salinity <- k1 + (k2 * (R^(1/2))) + (k3 * R) + (k4 * (R^(3/2))) + (k5 *( R^2)) + (k6 * (R^(5/2)))
+  
+  return(salinity)
+}
+
+covariates <- covariates %>% 
+  mutate(salinity = (salinity(covariates$salinity)))
+
+comm.data.north <- comm.data %>% 
+  filter(Site_ID > 600)
+
+comm.data.south <- comm.data %>% 
+  filter(Site_ID < 600)
 
 # calc shannon diversity for all 100 quads
 shannon_dune <- diversity(comm.data, index = "shannon", groups = comm.data$Site_ID, equalize.groups = F)
 
+shannon_dune.s <- diversity(comm.data.north, index = "shannon", groups = comm.data.north$Site_ID, equalize.groups = F)
+shannon_dune.n <- diversity(comm.data.south, index = "shannon", groups = comm.data.south$Site_ID, equalize.groups = F)
+
+
 model.df <- covariates %>% 
   mutate(shannon_diversity = shannon_dune,
          pct_cover = total_cover$total_cover_pct)
+
+for(i in c(8:11)){model.df[,i] <- (model.df[,i] - mean(model.df[,i]))/sd(model.df[,i])} 
+
 
 model.df.north <- model.df %>% 
   filter(n_s == "N")
@@ -31,7 +73,6 @@ model.df.south <- model.df %>%
 ## Which abiotic characteristics drive plant community diversity and cover? 
 ## Does this vary b/w North and South ends of Nannygoat Beach? 
 
-for(i in c(8:11)){model.df[,i] <- (model.df[,i] - mean(model.df[,i]))/sd(model.df[,i])} 
 
 #model.df.north %>% 
 
@@ -39,17 +80,19 @@ for(i in c(8:11)){model.df[,i] <- (model.df[,i] - mean(model.df[,i]))/sd(model.d
 #model.df.south %>% 
   
 
-dunecommrand <- lmer(shannon_diversity ~ dist.to.strand..m. + specific_conductance 
+dunecommrand <- lmer(shannon_diversity ~ salinity + dist.to.strand..m. 
                      + soil.moisture.pct + pct.OM + (1 | Site), data = model.df)
 
-anova(dunecommrand)
+report.dunemod <- report(dunecommrand)
 
-library(lme4)
-library(nlme)
-mod1 <- lme(shannon_diversity ~ dist.to.strand..m. + specific_conductance 
-            + soil.moisture.pct + pct.OM, random= ~1 | Site, method='ML', data = model.df)
-
-summary(dunecommrand)
+#anova(dunecommrand)
+#
+#library(lme4)
+#library(nlme)
+#mod1 <- lme(shannon_diversity ~ dist.to.strand..m. + specific_conductance 
+#            + soil.moisture.pct + pct.OM, random= ~1 | Site, method='ML', data = model.df)
+#
+#summary(dunecommrand)
 
 ###
 
@@ -59,7 +102,7 @@ confint <- confint[-c(1:3),]
 estimate_mod <- as.data.frame(coef(summary(dunecommrand)))
 estimate_mod <- estimate_mod[-1,]
 
-coef <- c("dist.to.strand..m.","specific_conductance", "soil.moisture.pct", "pct.OM")
+coef <- c("dist.to.strand..m.","salinity", "soil.moisture.pct", "pct.OM")
 
 confint <- cbind(confint, coef)
 colnames(confint) <- c("lowerci", "upperci", "coef")
@@ -84,12 +127,12 @@ ggplot(confint, aes(estimate, reorder(coef, -class))) +
 
 ###
 
-dunecommrand_north <- lmer(shannon_diversity ~ dist.to.strand..m. + specific_conductance 
+dunecommrand_north <- lmer(shannon_diversity ~ dist.to.strand..m. + salinity 
                            + soil.moisture.pct + pct.OM +(1 | Site), data = model.df.north)
 
 summary(dunecommrand_north)
 
-dunecommrand_south <- lmer(shannon_diversity ~ dist.to.strand..m. + specific_conductance 
+dunecommrand_south <- lmer(shannon_diversity ~ dist.to.strand..m. + salinity 
                            + soil.moisture.pct + pct.OM +(1 | Site), data = model.df.south)
 
 summary(dunecommrand_south)
@@ -102,35 +145,57 @@ summary(dunecommrand_south)
 
 # salinity
 
-var.test(specific_conductance ~ n_s, covariates, 
+var.test(salinity ~ n_s, covariates, 
          alternative = "two.sided")
 
-t.test(model.df.north$specific_conductance, model.df.south$specific_conductance, var.equal = F)
+ttest_salt <- t.test(model.df.north$salinity, model.df.south$salinity, var.equal = F)
+report(ttest_salt)
+t.test(log(model.df.north$salinity), log(model.df.south$salinity), var.equal = F)
 
-t.test(log(model.df.north$specific_conductance), log(model.df.south$specific_conductance), var.equal = F)
+boxplot(covariates$salinity ~ covariates$n_s, outline =F)
+boxplot(model.df$salinity ~ model.df$n_s, outline =F)
 
-boxplot(covariates$specific_conductance ~ covariates$n_s, outline =F)
+
+ttestplot_salt<-ggplot(covariates, aes(x=n_s, y=salinity, fill=n_s)) +
+  geom_boxplot() +
+  labs(x="Site Location",
+       y ="Practical Salinity Unit")
+ttestplot_salt
 
 # soil moisture - NOT CORRECT, NEED ALTERNATE TEST FOR NON-CONTINUOUS, NON-NORMAL DATA
 
 var.test(soil.moisture.pct ~ n_s, covariates, 
          alternative = "two.sided")
 
-t.test(model.df.north$soil.moisture.pct, model.df.south$soil.moisture.pct, var.equal = F)
-
-t.test(log(model.df.north$specific_conductance), log(model.df.south$specific_conductance), var.equal = F)
+ttest_sm <- t.test(model.df.north$soil.moisture.pct, model.df.south$soil.moisture.pct, var.equal = F)
+report(ttest_sm)
+t.test(log(model.df.north$salinity), log(model.df.south$salinity), var.equal = F)
 
 hist(model.df.$soil.moisture.pct)
+
+boxplot(model.df$soil.moisture.pct ~ model.df$n_s, outline =T)
+
+ttestplot_SM<-ggplot(covariates, aes(x=n_s, y=soil.moisture.pct, fill=n_s)) +
+  geom_boxplot() +
+  labs(x="Site Location",
+       y ="Soil Moisture %")
+ttestplot_SM
 
 # organic material - NOT CORRECT, NEED ALTERNATE TEST FOR NON-CONTINUOUS, NON-NORMAL DATA
 
 var.test(soil.moisture.pct ~ n_s, covariates, 
          alternative = "two.sided")
 
-t.test(model.df.north$pct.OM, model.df.south$pct.OM, var.equal = F)
+ttest_om <- t.test(model.df.north$pct.OM, model.df.south$pct.OM, var.equal = F)
+report(ttest_om)
 
-t.test(log(model.df.north$specific_conductance), log(model.df.south$specific_conductance), var.equal = F)
+boxplot(model.df$pct.OM ~ model.df$n_s, outline =F)
 
+ttestplot_OM<-ggplot(covariates, aes(x=n_s, y=pct.OM, fill=n_s)) +
+  geom_boxplot() +
+  labs(x="Site Location",
+       y ="Organic Matter %")
+ttestplot_OM
 
 
 ####  question 3 ####
@@ -139,33 +204,48 @@ t.test(log(model.df.north$specific_conductance), log(model.df.south$specific_con
 
 # all sites
 
-ggplot(model.df, aes(x = dist.to.dune..m., y = specific_conductance)) + 
+ggplot(model.df, aes(x = dist.to.dune..m., y = salinity)) + 
   geom_point() +
   stat_smooth(method = "lm") +
-  stat_cor(label.x = 60, label.y = 1700) +
-  stat_regline_equation(label.x = 60, label.y = 1625)
+  stat_cor(label.x = 60, label.y = 1) +
+  stat_regline_equation(label.x = 60, label.y = 1.1) +
+  labs(x = "Distance to Dune (m)",
+       y = "Practical Salinity Unit")
 
-summary(lm(model.df$specific_conductance ~ model.df$dist.to.dune..m.))
+lm_all <- lm(model.df$salinity ~ model.df$dist.to.dune..m.)
+report(lm_all)
+
+summary(lm(model.df$salinity ~ model.df$dist.to.dune..m.))
 
 # north site
 
-ggplot(model.df.north, aes(x = dist.to.dune..m., y = specific_conductance)) + 
+ggplot(model.df.north, aes(x = dist.to.dune..m., y = salinity)) + 
   geom_point() +
   stat_smooth(method = "lm") +
-  stat_cor(label.x = 60, label.y = 400) +
-  stat_regline_equation(label.x = 60, label.y = 380)
+  stat_cor(label.x = 60, label.y = 0.3) +
+  stat_regline_equation(label.x = 60, label.y = 0.32) +
+  labs(x = "Distance to Dune (m)",
+       y = "Practical Salinity Unit")
 
-summary(lm(model.df.north$specific_conductance ~ model.df.north$dist.to.dune..m.))
+lm_n <- lm(model.df.north$salinity ~ model.df.north$dist.to.dune..m.)
+report(lm_n)
+summary(lm(model.df.north$salinity ~ model.df.north$dist.to.dune..m.))
 
 # south site
 
-ggplot(model.df.south, aes(x = dist.to.dune..m., y = specific_conductance)) + 
+ggplot(model.df.south, aes(x = dist.to.dune..m., y = salinity)) + 
   geom_point() +
   stat_smooth(method = "lm") +
-  stat_cor(label.x = 60, label.y = 1600) +
-  stat_regline_equation(label.x = 60, label.y = 1545)
+  stat_cor(label.x = 60, label.y = 0.7) +
+  stat_regline_equation(label.x = 60, label.y = 0.75) +
+  labs(x = "Distance to Dune (m)",
+       y = "Practical Salinity Unit")
 
-summary(lm(model.df.south$specific_conductance ~ model.df.south$dist.to.dune..m.))
+  
+
+lm_s <- lm(model.df.south$salinity ~ model.df.south$dist.to.dune..m.)
+report(lm_s)
+summary(lm(model.df.south$salinity ~ model.df.south$dist.to.dune..m.))
 
 
 
@@ -187,15 +267,40 @@ ns_groups_n <- ns_groups %>%
 ns_groups_s <- ns_groups %>% 
   filter(n_s == "S")
 
-t.test(ns_groups_n$shannon_index, ns_groups_s$shannon_index, var.equal = F)
+report(t.test(ns_groups_n$shannon_index, ns_groups_s$shannon_index, var.equal = F))
+
+t.test(ns_groups_n$shannon_index, ns_groups_s$shannon_index, var.equal = T)
+
+ttestplot_div<-ggplot(ns_groups, aes(x=n_s, y=shannon_index, fill=n_s)) +
+  geom_boxplot() +
+  labs(x = "Site Location",
+       y = "Shannon Diversity Index")
+ttestplot_div
+
+boxplot(ns_groups$shannon_index ~ ns_groups$n_s)
+boxplot(covariates$salinity ~ covariates$n_s, outline =T)
+
+
+
 
 # cca analysis
 
-duneCCA <- cca(comm.data ~ dist.to.strand..m. + specific_conductance + soil.moisture.pct + pct.OM, data = model.df)
+duneCCA <- cca(comm.data ~ dist.to.strand..m. + salinity + soil.moisture.pct + pct.OM, data = model.df)
 duneCCA
 summary(duneCCA)
 
 duneCCAplot <- plot(duneCCA)
+duneCCAplot
+
+x <- plot(duneCCA)
+geom_text_repel(data = sp, mapping = aes(x = CCA1, y = CCA2, label = Label))
+
+sp <- fortify(duneCCA, display = "sp")
+
+
+plot(duneCCA, display=c("sp","lc","cn"), scaling=2, xlab="CCA1 ()", ylab="CCA2 ()")
+
+
 
 # is CCA significant?
 anova(duneCCA)
@@ -205,3 +310,123 @@ anova(duneCCA, by = "axis")
 
 # are vectors significant?
 anova(duneCCA, by = "term", permutations = 999)
+
+#cca north
+
+
+
+duneCCA.north <- cca(comm.data.north ~ dist.to.strand..m. + salinity + soil.moisture.pct + pct.OM, data = model.df.north)
+duneCCA.north
+summary(duneCCA.north)
+
+duneCCAplot.north <- plot(duneCCA.north)
+
+# is CCA significant?
+anova(duneCCA.north)
+
+# are axes significant?
+anova(duneCCA.north, by = "axis")
+
+# are vectors significant?
+anova(duneCCA.north, by = "term", permutations = 999)
+
+#cca south
+
+
+duneCCA.south<- cca(comm.data.south ~ dist.to.strand..m. + salinity + soil.moisture.pct + pct.OM, data = model.df.south)
+duneCCA.south
+summary(duneCCA.north)
+
+duneCCAplot.south <- plot(duneCCA.south)
+
+# is CCA significant?
+anova(duneCCA.south)
+
+# are axes significant?
+anova(duneCCA.south, by = "axis")
+
+# are vectors significant?
+anova(duneCCA.south, by = "term", permutations = 999)
+
+
+### testing lmer for N & S
+
+dunecommrand_north <- lmer(shannon_diversity ~ dist.to.strand..m. + salinity 
+                           + soil.moisture.pct + pct.OM +(1 | Site), data = model.df.north)
+plot(parameters(dunecommrand_north))
+
+summary(dunecommrand_north)
+
+results.n <- psycho::analyze(dunecommrand_north, CI = 95)
+
+dunecommrand_south <- lmer(shannon_diversity ~ salinity + dist.to.strand..m. 
+                           + soil.moisture.pct + pct.OM +(1 | Site), data = model.df.south)
+plot(parameters(dunecommrand_south))
+
+plot(parameters(dunecommrand))
+
+
+
+report.s <- report(dunecommrand_south)
+
+report(dunecommrand)
+
+confint <- as.data.frame(confint(dunecommrand_south)) 
+confint <- confint[-c(1:3),]
+
+estimate_mod <- as.data.frame(coef(summary(dunecommrand_south)))
+estimate_mod <- estimate_mod[-1,]
+
+coef <- c("dist.to.strand..m.","salinity", "soil.moisture.pct", "pct.OM")
+
+confint <- cbind(confint, coef)
+colnames(confint) <- c("lowerci", "upperci", "coef")
+confint <- confint %>% 
+  mutate(estimate = estimate_mod$Estimate,
+         class = c(1:4))
+
+plot(parameters(dunecommrand))
+report(dunecommrand) %>% 
+  summary()
+summary(as.data.frame(report_all))
+
+
+# plant community plots 
+plant_comms_vis <- read.csv("data/plant_communities.csv")
+
+
+plant_comm_plot <- ggplot(plant_comms_vis, aes(fill=Species, y=Occurrence, x=Site)) + 
+  geom_bar(position="stack", stat="identity")
+plant_comm_plot
+
+
+# Stacked
+ggplot(data, aes(fill=condition, y=value, x=specie)) + 
+  geom_bar(position="stack", stat="identity")
+
+
+plot(parameters(dunecommrand_north))
+plot(parameters(dunecommrand_south))
+
+report(dunecommrand) %>% 
+  summary()
+
+sjPlot:: tab_model(dunecommrand)
+
+
+sjPlot::tab_model(dunecommrand, 
+                  show.re.var= TRUE, 
+                  pred.labels =c("(Intercept)", "Salinity", "Dist. to Stand (m)", "Soil Moisture (%)", "Org. Material (%)"),
+                  dv.labels= "Effects of Abiotic Factors on Shannon Diversity")
+
+report(dunecommrand_north) %>% 
+  summary()
+
+sjPlot::tab_model(dunecommrand_north)
+
+
+report(dunecommrand_south) %>% 
+  summary()
+sjPlot::tab_model(dunecommrand_south)
+
+
